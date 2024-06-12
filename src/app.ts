5db2556f6713c7483a9c0faaab7386b29a831da8
Luciano Figueroa <lumusitech@gmail.com>
2024-06-11 22:43:40 -0300

    fix: add host to server creation to avoid connection error from postman

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ host: '0.0.0.0', port: 3000 });
 
 wss.on('connection', function connection(ws) {
+  console.log("client connected");
+
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
 
-  ws.send('something');
+  ws.send('Hello from Server!');
+
 });
+
+console.log('ws://localhost:3000');
